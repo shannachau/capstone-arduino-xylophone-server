dfd8ad466cae79b0b3319999ea9bdad8a7b3b375
Shanna Chau <shannachau@gmail.com>
2015-10-16 15:33:09 -0700

    Changing console log message

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ io.on('connection', function(socket){
 
   socket.on('testing', function(test){
     io.emit('testing', test);
-    console.log('test actually fucking works');
+    console.log(test);
   });
   console.log('someone is on the port');
 });
