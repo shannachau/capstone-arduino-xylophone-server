a1f5de4e5f005e100adde6f7d99c2a9c2da90dd9
Shanna Chau <shannachau@gmail.com>
2015-10-18 21:56:05 -0700

    DRYing up code into single call

--- server.js
+++ server.js
@@ -9,45 +9,45 @@ io.on('connection', function(socket){
   //   io.emit('key pressed', msg);
   // });
 
-  socket.on('servo2', function(test){
-    io.emit('servo2', test);
-    console.log('Moving servo2');
+  socket.on('moveServo', function(servo){
+    io.emit('moveServo', servo);
+    console.log('Moving ' + servo);
   });
 
-  socket.on('servo3', function(test){
-    io.emit('servo3', test);
-    console.log('Moving servo3');
-  });
-
-  socket.on('servo4', function(test){
-    io.emit('servo4', test);
-    console.log('Moving servo4');
-  });
-
-  socket.on('servo5', function(test){
-    io.emit('servo5', test);
-    console.log('Moving servo5');
-  });
-
-  socket.on('servo6', function(test){
-    io.emit('servo6', test);
-    console.log('Moving servo6');
-  });
-
-  socket.on('servo8', function(test){
-    io.emit('servo8', test);
-    console.log('Moving servo8');
-  });
-
-  socket.on('servo9', function(test){
-    io.emit('servo9', test);
-    console.log('Moving servo9');
-  });
-
-  socket.on('servo10', function(test){
-    io.emit('servo10', test);
-    console.log('Moving servo10');
-  });
+  // socket.on('servo3', function(test){
+  //   io.emit('servo3', test);
+  //   console.log('Moving servo3');
+  // });
+  //
+  // socket.on('servo4', function(test){
+  //   io.emit('servo4', test);
+  //   console.log('Moving servo4');
+  // });
+  //
+  // socket.on('servo5', function(test){
+  //   io.emit('servo5', test);
+  //   console.log('Moving servo5');
+  // });
+  //
+  // socket.on('servo6', function(test){
+  //   io.emit('servo6', test);
+  //   console.log('Moving servo6');
+  // });
+  //
+  // socket.on('servo8', function(test){
+  //   io.emit('servo8', test);
+  //   console.log('Moving servo8');
+  // });
+  //
+  // socket.on('servo9', function(test){
+  //   io.emit('servo9', test);
+  //   console.log('Moving servo9');
+  // });
+  //
+  // socket.on('servo10', function(test){
+  //   io.emit('servo10', test);
+  //   console.log('Moving servo10');
+  // });
 
   console.log('someone is on the port');
 });
